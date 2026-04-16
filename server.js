1a9ff904b6c4be33e6a67d1340680573756a028c
prateekhalwe <halwe.prateek@gmail.com>
2026-04-16 05:54:37 +0530

    Fix server routes

--- server.js
+++ server.js
@@ -3,8 +3,21 @@ const app = express();
 
 app.use(express.json());
 
+// Home route
 app.get("/", (req, res) => {
-    res.send("PrintToHome Backend Running");
+    res.send("PrintToHome backend is running 🚀");
+});
+
+// Temporary orders route
+app.get("/orders", (req, res) => {
+    res.json([
+        {
+            id: 1,
+            name: "Sample Order",
+            pages: 10,
+            status: "Processing"
+        }
+    ]);
 });
 
 app.listen(5000, () => {
